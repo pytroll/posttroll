ee88d9d3c705b2f56c01f0358d36df533e516296
Martin Raspaud <martin.raspaud@smhi.se>
2012-10-17 19:01:21 +0200

    Doc: remove mock

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,12 +19,6 @@ import sys, os
 #sys.path.insert(0, os.path.abspath('.'))
 
 
-import mock
- 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'zmq']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
