f802711732bc7f58a3ac0062b08e54c9aadc138c
Martin Raspaud <martin.raspaud@smhi.se>
2012-10-17 11:55:48 +0200

    rtd compatibility?

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,6 +18,13 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'zmq']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
