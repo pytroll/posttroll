9fbdcf2d695333c7d52ca47dfa6cdcadf2c2a871
Martin Raspaud <martin.raspaud@smhi.se>
2025-01-30 10:39:43 +0100

    Include nameserver in scripts

--- posttroll/ns.py
+++ posttroll/ns.py
@@ -137,3 +137,119 @@ class NameServer:
     def stop(self):
         """Stop the nameserver."""
         return self._ns.stop()
+
+
+def main():
+    """Run the nameserver script."""
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--daemon", help="Run as a daemon",
+                        choices=["start", "stop", "status", "restart"])
+    parser.add_argument("-l", "--log", help="File to log to (defaults to stdout)",
+                        default=None)
+    parser.add_argument("-v", "--verbose", help="print debug messages too",
+                        action="store_true")
+    parser.add_argument("--no-multicast", help="disable multicasting",
+                        action="store_true")
+    parser.add_argument("-L", "--local-only", help="accept connections only from localhost",
+                        action="store_true")
+    opts = parser.parse_args()
+
+    logger = setup_logging(opts)
+    multicast_enabled = not opts.no_multicast
+    local_only = (opts.local_only)
+
+    ns = NameServer(multicast_enabled=multicast_enabled, restrict_to_localhost=local_only)
+
+    if opts.daemon is None:
+        run(ns, logger)
+
+    else:  # Running as a daemon
+        run_as_daemon(ns, opts)
+
+
+def setup_logging(opts):
+    """Set up logging."""
+    if opts.log:
+        import logging.handlers
+        handler = logging.handlers.TimedRotatingFileHandler(opts.log,
+                                                            "midnight",
+                                                            backupCount=7)
+    else:
+        handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("[%(levelname)s: %(asctime)s :"
+                                           " %(name)s] %(message)s",
+                                           "%Y-%m-%d %H:%M:%S"))
+    if opts.verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+    handler.setLevel(loglevel)
+    logging.getLogger("").setLevel(loglevel)
+    logging.getLogger("").addHandler(handler)
+    return logging.getLogger("nameserver")
+
+
+
+def run(ns, logger):
+    """Run a nameserver process."""
+    try:
+        ns.run()
+    except KeyboardInterrupt:
+        pass
+    except:
+        logger.exception("Something wrong happened...")
+        raise
+    finally:
+        print("Thanks for using pytroll/nameserver. "
+              "See you soon on www.pytroll.org!")
+
+
+def run_as_daemon(ns, opts):
+    """Run nameserver as a daemon."""
+    if opts.daemon == "status":
+        import os
+        import sys
+        if os.path.exists("/tmp/nameserver.pid"):
+            with open("/tmp/nameserver.pid") as fd_:
+                pid = int(fd_.read())
+                try:
+                    os.kill(pid, 0)
+                except OSError:
+                    sys.exit(1)
+                else:
+                    sys.exit(0)
+        else:
+            sys.exit(1)
+    try:
+        import signal
+        import sys
+
+        import daemon.runner
+
+        class App(object):
+            """App object for running the nameserver as daemon."""
+            stdin_path = "/dev/null"
+            stdout_path = "/dev/null"
+            stderr_path = "/dev/null"
+            run = ns.run
+            pidfile_path = "/tmp/nameserver.pid"
+            pidfile_timeout = 90
+
+        def _terminate(*args):
+            """Terminate the nameserver."""
+            del args
+            ns.stop()
+
+        signal.signal(signal.SIGTERM, _terminate)
+
+        APP = App()
+        sys.argv = [sys.argv[0], opts.daemon]
+        angel = daemon.runner.DaemonRunner(APP)
+        if opts.log:
+            angel.daemon_context.files_preserve = [handler.stream]
+        angel.parse_args([sys.argv[0], opts.daemon])
+        sys.exit(angel.do_action())
+    except ImportError:
+        print("Cannot run as a daemon, you need python-daemon installed.")
